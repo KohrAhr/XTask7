5260a189b95a938fb3c11aef20190f13b9804aff
ZAM <ZAM@1CLICK.LV>
2024-01-30 18:22:25 +0200

    Initial commit

---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,22 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = new WinFormsApp3.Window1();
+            x.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ WinFormsApp3/Form2.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms.Integration;
+
+namespace WinFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
